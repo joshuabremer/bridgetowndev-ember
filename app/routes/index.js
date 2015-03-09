7c5b34c6d01b6ace6e6d59d183a6f39d8921d2d2
Josh Bremer <jbremer@sweetspotdiabetes.com>
2015-03-08 20:37:59 -0700

    Grabbing news from home page

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,15 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    // Do nothing...
+    var host = "http://www.bridgetowncomedy.com";
+    if ( window.location.host === "localhost:4000" ) {
+      host = "http://localhost:4000";
+    }
+
+    return Ember.$.ajax( host + "/")
+      .then(function(data) {
+        return $(data).find("#jekyll-content").html();
+    });
   },
 
   renderTemplate: function() {
