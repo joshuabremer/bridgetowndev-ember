5b475c35aa3b85a53d4271a6fce322e3b4cfb3b3
Josh Bremer <jbremer@sweetspotdiabetes.com>
2015-04-10 09:08:39 -0700

    Updated performers and shows

--- _node_scripts/build.js
+++ _node_scripts/build.js
@@ -7,24 +7,24 @@ var Q = require( "q" );
 var util = require( "./utilities" );
 
 TMP_EVENTS_PATH = festivalData.tmpEventsPath;
-////http://bridgetown.festivalthing.com/export/events/json
-FESTIVALTHING_EVENTS_URL = "http://127.0.0.1:4200/fixtures/festivalthing-events.json";
+// FESTIVALTHING_EVENTS_URL = "http://bridgetown.festivalthing.com/export/events/json"
+// FESTIVALTHING_EVENTS_URL = "http://127.0.0.1:4200/fixtures/festivalthing-events.json";
 
 TMP_VENUES_PATH = festivalData.tmpVenuesPath;
 ////http://bridgetown.festivalthing.com/export/venues/json
-FESTIVALTHING_VENUE_URL = "http://127.0.0.1:4200/fixtures/festivalthing-venues.json";
+//FESTIVALTHING_VENUE_URL = "http://127.0.0.1:4200/fixtures/festivalthing-venues.json";
 
 TMP_SCHEDULE_PATH = festivalData.tmpSchedulePath;
 ////http://bridgetown.festivalthing.com/export/schedule/json
 FESTIVALTHING_SCHEDULE_URL = "http://127.0.0.1:4200/fixtures/festivalthing-schedule.json";
 
 TMP_PERFORMERS_PATH = festivalData.tmpPerformersPath;
-//http://bridgetown.festivalthing.com/export/performers/json
 FESTIVALTHING_PERFORMERS_URL = "http://bridgetown.festivalthing.com/export/performers/json";
+//FESTIVALTHING_PERFORMERS_URL = "http://bridgetown.festivalthing.com/export/performers/json";
 
 TMP_SHOWS_PATH = festivalData.tmpShowsPath;
-////http://bridgetown.festivalthing.com/export/submitted-shows/json
 FESTIVALTHING_SHOWS_URL = "http://bridgetown.festivalthing.com/export/submitted-shows/json";
+//FESTIVALTHING_SHOWS_URL = "http://bridgetown.festivalthing.com/export/submitted-shows/json";
 
 
 Q.all([
